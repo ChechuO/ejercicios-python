9fe6335d5002b845d87c1a2a99fe69f24f686f0f
Chechu <chechuolivar@gmail.com>
2026-07-07 09:50:00 +0200

    Este es el primer ejercicio

---
+++ sintaxis-variables-tipos-datos.py
@@ -0,0 +1,25 @@
+"""
+Este es un ejercicio hecho en Python
+https://www.python.org/
+Esto es un comentario en varias lineas
+"""
+
+# Esto es un comentario
+
+'''
+Esto tambien es un comentario
+en varias lineas
+'''
+
+variable = 25 # esto es una variable
+
+CONSTANTE = 67 # esto es una constante por convencion
+
+tipo_int = 1 # Esto es de tipo int
+tipo_str = "hola" # Esto es de tipo str
+tipo_bool = True # Esto es de tipo bool
+tipo_float = 1.25 # Esto es de tipo float
+lenguaje = 'python' # Esto es de tipo str
+
+print(f"hola, {lenguaje}")
+
